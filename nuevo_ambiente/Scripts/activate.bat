4511870a1d2b8a24cc58c07531f65ef7f6e5689c
Manuel <mzueta@unal.edu.co>
2025-03-02 20:31:32 -0500

    ingresamos el tablero de power bi

--- nuevo_ambiente/Scripts/activate.bat
+++ nuevo_ambiente/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\manue\OneDrive\Desktop\proyecto\Hocicones\hocicones
+set VIRTUAL_ENV=C:\Users\manue\OneDrive\Desktop\Hocicones\Hocicones\nuevo_ambiente
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(hocicones) %PROMPT%
+set PROMPT=(nuevo_ambiente) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(hocicones) 
+set VIRTUAL_ENV_PROMPT=(nuevo_ambiente) 
 
 :END
 if defined _OLD_CODEPAGE (
